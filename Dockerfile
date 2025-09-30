868148e3c46f0eeb54118632f3440e62273054b1
ocean1man <email@notneeded.com>
2025-09-30 11:18:44 +0300

    change DBMS

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.12.6-alpine
 
+ENV PYTHONDONTWRITEBYTECODE=1 \
+    PYTHONUNBUFFERED=1
+
 WORKDIR /app
 
 COPY requirements.txt ./
@@ -8,6 +11,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 8000
-
-ENTRYPOINT ["./entrypoint.sh"]
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- bookshop/management/commands/import_data.py
+++ bookshop/management/commands/import_data.py
@@ -1,83 +1,84 @@
-import sqlite3
 import csv
 from django.core.management.base import BaseCommand
+from django.db import transaction
+from bookshop.models import Book, AgeLimit, Author, Genre
 
 
 class Command(BaseCommand):
     @staticmethod
+    @transaction.atomic
     def main():
-        csv_file_path_genres = 'data/genres.csv'
-        csv_file_path_authors = 'data/authors.csv'
-        csv_file_path_books = 'data/books.csv'
-        csv_file_path_books_genres = 'data/books_genres.csv'
-        csv_file_path_admin = 'data/admin.csv'
-        csv_file_path_agelimits = 'data/age_limits.csv'
+        CSV_FILE_PATH_GENRES = 'data/genres.csv'
+        CSV_FILE_PATH_AUTHORS = 'data/authors.csv'
+        CSV_FILE_PATH_BOOKS = 'data/books.csv'
+        CSV_FILE_PATH_BOOKS_GENRES = 'data/books_genres.csv'
+        CSV_FILE_PATH_AGELIMITS = 'data/age_limits.csv'
 
-        conn = sqlite3.connect('db.sqlite3')
-        cursor = conn.cursor()
-
-        with open(csv_file_path_genres, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(CSV_FILE_PATH_AGELIMITS, 'r', newline='', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
-            header = next(csvreader)
-
+            next(csvreader)
             for row in csvreader:
-                cursor.execute('''
-                INSERT INTO bookshop_genre (name)
-                VALUES (?)
-                ''', (row[0],))
+                AgeLimit.objects.get_or_create(
+                    id=row[0],
+                    defaults={"value": row[1]},
+                )
 
-        with open(csv_file_path_authors, 'r', newline='', encoding='utf-8') as csvfile:
+        print("Возрастные ограничения загружены")
+        
+        with open(CSV_FILE_PATH_AUTHORS, 'r', newline='', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
-            header = next(csvreader)
-
+            next(csvreader)
             for row in csvreader:
-                cursor.execute('''
-                INSERT INTO bookshop_author (fullname)
-                VALUES (?)
-                ''', (row[0],))
+                Author.objects.get_or_create(
+                    id=row[0],
+                    defaults={"fullname": row[1]},
+                )
+        print("Авторы загружены")
 
-        with open(csv_file_path_agelimits, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(CSV_FILE_PATH_GENRES, 'r', newline='', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
-            header = next(csvreader)
-
+            next(csvreader)
             for row in csvreader:
-                cursor.execute('''
-                INSERT INTO bookshop_agelimit (value)
-                VALUES (?)
-                ''', (row[0],))
+                Genre.objects.get_or_create(
+                    id=row[0],
+                    defaults={"name": row[1]},
+                )
+        print("Жанры загружены")
 
-        with open(csv_file_path_books, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(CSV_FILE_PATH_BOOKS, 'r', newline='', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
-            header = next(csvreader)
-
+            next(csvreader)
             for row in csvreader:
-                cursor.execute('''
-                INSERT INTO bookshop_book (title, year, ISBN, age_limit_id, price, count, description, author_id, img_path)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8],))
+                age_limit = AgeLimit.objects.filter(id=row[4]).first()
+                author = Author.objects.filter(id=row[8]).first()
+                Book.objects.get_or_create(
+                    id=row[0],
+                    defaults={
+                        "title": row[1],
+                        "year": row[2],
+                        "ISBN": row[3],
+                        "age_limit": age_limit,
+                        "price": row[5],
+                        "count": row[6],
+                        "description": row[7],
+                        "author": author,
+                        "img_path": row[9],
+                    },
+                )
+        print("Книги загружены")
 
-        with open(csv_file_path_books_genres, 'r', newline='', encoding='utf-8') as csvfile:
+        with open(CSV_FILE_PATH_BOOKS_GENRES, 'r', newline='', encoding='utf-8') as csvfile:
             csvreader = csv.reader(csvfile)
-            header = next(csvreader)
-
+            next(csvreader)
+            objs = []
+            Through = Book.genres.through
             for row in csvreader:
-                cursor.execute('''
-                INSERT INTO bookshop_book_genres (book_id, genre_id)
-                VALUES (?, ?)
-                ''', (row[0], row[1],))
+                book_id, genre_id = row[1], row[2]
 
-        with open(csv_file_path_admin, 'r', newline='', encoding='utf-8') as csvfile:
-            csvreader = csv.reader(csvfile)
-            header = next(csvreader)
+                objs.append(Through(id=row[0], book_id=book_id, genre_id=genre_id))
 
-            for row in csvreader:
-                cursor.execute('''
-                INSERT INTO auth_user (password, last_login, is_superuser, username, last_name, email, is_staff, is_active, date_joined, first_name)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                ''', (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],))
-
-        conn.commit()
-        conn.close()
+            Through.objects.bulk_create(objs, ignore_conflicts=True)
+        print("Связи книги-жанры загружены")
 
         print("Данные успешно импортированы")
 
